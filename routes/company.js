8a364e49770ee0bbe873101a1d034d11a1687c2d
Majoro Futhoane <85506666+AzazelEmjay@users.noreply.github.com>
2022-09-22 14:26:41 +0200

    route for getting single company

--- routes/company.js
+++ routes/company.js
@@ -13,5 +13,24 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', getCompany, (req, res) => {
+    res.json(res.company)  
+})
+
+
+async function getCompany(req, res, next){
+    let company
+    try {
+        company = await Company.findById(req.params.id)
+        if (company == null){
+            return res.status(404).json({message: 'Company not found' })
+        }
+    }
+    catch(err){
+        return res.status(500).json({message: err.message})
+    }
+    res.company = company
+    next()
+} 
 module.exports = router
 
