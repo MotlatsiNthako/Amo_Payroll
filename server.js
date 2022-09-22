9927d050c25482393eb2ac0ca01586ff4f0e2f54
Majoro Futhoane <85506666+AzazelEmjay@users.noreply.github.com>
2022-09-22 13:42:31 +0200

    Creating a server page

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error)=> console.error(error))
+db.once('open',(error)=> console.log('Connected to Database'))
+
+app.use(express.json())
+
+const loginRouter = require('./routes/login')
+app.use('/login', loginRouter)
+
+
+
+
+app.listen(3000, () => console.log('Server Started'))
