f9971af70c53ba0f8a800e474120c91ccad06b12
Majoro Futhoane <85506666+AzazelEmjay@users.noreply.github.com>
2022-09-22 13:39:50 +0200

    Creating a get one user route

--- routes/login.js
+++ routes/login.js
@@ -12,6 +12,25 @@ router.get('/', async (req, res) => {
         res.status(500).json({ message: err.message})
     }
 })
+//Getting One
+router.get('/:id', getUser, (req, res) => {
+     res.json(res.user)  
+})
 
+
+async function getUser(req, res, next){
+    let user
+    try {
+        user = await User.findById(req.params.id)
+        if (user == null){
+            return res.status(404).json({message: 'User not found' })
+        }
+    }
+    catch(err){
+        return res.status(500).json({message: err.message})
+    }
+    res.user = user
+    next()
+} 
 module.exports = router
 
