c72d4f27c8c915fecef61b569c1702ee87f17e14
Majoro Futhoane <85506666+AzazelEmjay@users.noreply.github.com>
2022-09-22 13:37:26 +0200

    Creating a route for getting all users

---
+++ routes/login.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/logins')
+
+
+//Getting all
+router.get('/', async (req, res) => {
+    try{
+        const users = await User.find()
+       res.json (users)
+    } catch(err){
+        res.status(500).json({ message: err.message})
+    }
+})
+
+module.exports = router
+
